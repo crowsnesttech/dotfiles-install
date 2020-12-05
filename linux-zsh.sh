62afec03a2d762ecddb06f41ab5dc3b80047b197
CrowsNestTech <crowsnesttech@gmail.com>
2020-12-05 16:00:24 -0700

    update

---
+++ linux-zsh.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+mkdir /home/nick/Documents/git
+mkdir /home/nick/Documnets/git/dotfiles
+cd /home/nick/Documents/git/dotfiles/
+echo "Clone Linux-Dotfiles from Github"
+git clone https://github.com/crowsnesttech/linux-dotfiles.git
+
+echo "removing old files"
+rm /home/nick/.oh-my-zsh
+rm /home/nick/.vimrc
+rm /home/nick/.zshrc
+echo "creating symlinks"
+sleep 2
+
+ln -s /home/nick/Documents/git/linux-dotfiles/.oh-my-zsh ~/
+ln -s /home/nick/Documents/git/linux-dotfiles/.vimrc ~/
+ln -s /home/nick/Documents/git/linux-dotfiles/.zshrc ~/
+
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
