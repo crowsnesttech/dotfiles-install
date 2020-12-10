f863de2a67532f48e31c287974c4dd5be570daa0
Nick Ryan <crowsnesttech@gmail.com>
2020-12-10 06:35:54 -0700

    update121020

--- linux-zsh.sh
+++ linux-zsh.sh
@@ -33,3 +33,5 @@ sleep 1
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+chsh -s $(which zsh)
