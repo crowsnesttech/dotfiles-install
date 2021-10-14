323c5c7c480871830ab578be3f50f07f1afc2a5b
Nick Ryan <crowsnesttech@gmail.com>
2021-10-14 10:29:47 -0600

    VariableUser

--- linux-zsh.sh
+++ linux-zsh.sh
@@ -3,38 +3,38 @@
 sudo apt update
 sudo apt install zsh -y 
 
-mkdir -p /home/nick/Documents/git/dotfiles/linux-dotfiles
+mkdir -p /home/$USER/Documents/git/dotfiles/linux-dotfiles
 sleep 1
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "Clone Linux-Dotfiles from Github"
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 
-rm -rf /home/nick/Documents/git/dotfiles/linux-dotfiles/
+rm -rf /home/$USER/Documents/git/dotfiles/linux-dotfiles/
 
-git clone https://github.com/crowsnesttech/linux-dotfiles.git /home/nick/Documents/git/dotfiles/linux-dotfiles/
+git clone https://github.com/crowsnesttech/linux-dotfiles.git /home/$USER/Documents/git/dotfiles/linux-dotfiles/
 
-git clone https://github.com/ohmyzsh/ohmyzsh.git /home/nick/Documents/git/dotfiles/linux-dotfiles/.oh-my-zsh/
+git clone https://github.com/ohmyzsh/ohmyzsh.git /home/$USER/Documents/git/dotfiles/linux-dotfiles/.oh-my-zsh/
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "removing old files"
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 sleep 1
 
-rm /home/nick/.oh-my-zsh
-rm /home/nick/.vimrc
-rm /home/nick/.zshrc
-rm /home/nick/.tmux.conf
+rm /home/$USER/.oh-my-zsh
+rm /home/$USER/.vimrc
+rm /home/$USER/.zshrc
+rm /home/$USER/.tmux.conf
 
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "creating symlinks"
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 sleep 1
 
-ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.oh-my-zsh ~/
-ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.vimrc ~/
-ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.zshrc ~/
-ln -s /home/nick/Documents/git/dotfiles/linux-dotfiles/.tmux.conf ~/
+ln -s /home/$USER/Documents/git/dotfiles/linux-dotfiles/.oh-my-zsh ~/
+ln -s /home/$USER/Documents/git/dotfiles/linux-dotfiles/.vimrc ~/
+ln -s /home/$USER/Documents/git/dotfiles/linux-dotfiles/.zshrc ~/
+ln -s /home/$USER/Documents/git/dotfiles/linux-dotfiles/.tmux.conf ~/
 sleep 1
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -51,5 +51,5 @@ git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:
 sudo chsh -s $(which zsh)
 echo "=========================================="
 echo "REMEMBER TO LOGOUT OF COMPUTER TO ENABLE SHELL"
-echo "pop_os remember to change the font in the terminal app to DejaVu Sans Mono for Powerline Book"
+echo "POP_OS --> remember to change the font in the terminal app to DejaVu Sans Mono for Powerline Book"
 echo "=========================================="
