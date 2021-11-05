8297f82b0a687efa46bf2722ad6ba54d4bc4f189
CrowsNestTech <crowsnesttech@gmail.com>
2021-11-05 02:11:37 +0000

    added iterm install

--- macos-zsh.sh
+++ macos-zsh.sh
@@ -31,6 +31,7 @@ ln -s $HOME/git/dotfiles/macos-dotfiles/.zshrc ~/
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
 echo "Adding ZSH Plugins"
 echo "+++++++++++++++++++++++++++++++++++++++++++++"
+brew install zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 cp $HOME/git/dotfiles/macos-dotfiles/agnostercstm.zsh-theme $HOME/git/dotfiles/macos-dotfiles/.oh-my-zsh/custom/themes/
